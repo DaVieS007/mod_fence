5a9453ad9c34de4a644dd1efe3140bd446918f71
root <root@localhost.localdomain>
2016-11-30 04:19:55 +0100

    + Added Soft and Hard Mitigation against flood + Added Hostname validation + Added Custom Error Handling with Status Page Version: 0.9b

--- README.md
+++ README.md
@@ -1,28 +1,53 @@
 # mod_fence
-## Version: 0.1 (alpha)
+## Version: 0.9b (beta)
 
-##### With mod_fence is under developing for reason to do guarding apache against DOS attacks and any known bugs.
+#### Primary Tasks:
+**This module has only one primary task to covering apache with additional fences to protect against:**
 
-In this early stage mod_fence is only can do the followings:
+- [x] Hostname validity checking by https://tools.ietf.org/html/rfc3986
+- [x] Unresponsible or very slow website (hitting F5 like shooting in Call Of Duty)
+- [x] Also Good against many well known attacks like: slowloris
+- [x] PHP/Session FLOCK() de-stucking, yes its destuck apache modules!
 
-Try to kill stucked childs TESTED ON: (MPM_ITK, MPM_PREFORK) 
+#### ** Currently Tested on Apache2.4 MPM_ITK / MPM_PREFORK **
+##### Main advantage is very lightweight and uses Apache Core Libs.
 
-**Known PHP Bug is apache's children get stucked while PHP indefinitely try to access session file via FLOCK(...)**
+
+## History:
+- **Known PHP Bug is apache's children get stucked while PHP indefinitely try to access session file via FLOCK(...) that simply causes a lot of headache for me**
+- **Continuously pressing F5 on website which is under heavy load is a lamer things and must be avoid**
+- **On shared hosting slow or mysql (write_lock) stucked websites able to denial the service**
+- **There is a method to kill the apache with connections from ADSL with small packets and does not need to flood**
 
 ## Configuration
 
 ````
 <IfModule mod_fence.c>
-  Fence_Enable          On
-  Fence_ChildTimeout    3600 # Dont set it too small, because cause stucks
+    Fence_Enable          On
+    Fence_ChildTimeout    3600
+    Fence_MitigateSoftRequests 5
+    Fence_MitigateHardRequests 15
 </IfModule>
 ````
 
+#### Configuration Parameters
+- **Fence_Enable (On/Off)** *(Enables the module)*
+- **Fence_ChildTimeout ((numeric) seconds)** *(Sends a SIGKILL to child if not responding within the time)*
+- **Fence_MitigateSoftRequests ((numeric) requests)** *(Mitigate a single IP when stalled >= (1s) requests reach this limit at same time)*
+- **Fence_MitigateHardRequests ((numeric) requests)** *(Mitigate a single IP when any requests reach this limit at same time)*
+
+#### In case of Apache behind proxy
+###### When Apache behind the proxy its recommended to resolv true IPs through X-Forwarded-For header. There are already some modules to doing that, like mod_rpaf2
+
+
 ## License
 ````
-
+/*
    Copyright 2016 Hlavaji Viktor / DaVieS
         nPulse.net / davies@npulse.net
+    
+    Thanks to Systech Global Ltd (systech.hu) to actively support this project
+
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -35,5 +60,5 @@ Try to kill stucked childs TESTED ON: (MPM_ITK, MPM_PREFORK)
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
+*/
 ````
--- mod_fence.c
+++ mod_fence.c
@@ -1,6 +1,9 @@
 /*
    Copyright 2016 Hlavaji Viktor / DaVieS
         nPulse.net / davies@npulse.net
+    
+    Thanks to Systech Global Ltd (systech.hu) to actively support this project
+
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,6 +18,15 @@
    limitations under the License.
 */
 
+/* This module has only one primary task to covering apache with additional fences
+    to protect against:
+
+    - Hack (like: hostname: () {})
+    - Unresponsible or very slow website (hitting F5 like shooting in Call Of Duty)
+    - Also Good against many well known attacks like: slowloris
+    - PHP/Session FLOCK() stucking
+*/
+
 #include "ap_release.h"
 #if AP_SERVER_MAJORVERSION_NUMBER >= 2 && AP_SERVER_MINORVERSION_NUMBER >= 4
   #define DEF_IP   useragent_ip
@@ -25,6 +37,8 @@
   #define DEF_ADDR connection->remote_addr
   #define DEF_POOL connection->pool
 #endif
+
+#define MAX_HOSTNAME_LEN 255 //https://tools.ietf.org/html/rfc3986
 
 #include "httpd.h"
 #include "http_config.h"
@@ -40,7 +54,7 @@
 #include "scoreboard.h"
 #include "http_log.h"
 
-#define VERSION "mod_fence/0.2a"
+#define VERSION "mod_fence/0.9b"
 
 #if APR_HAVE_UNISTD_H
 #include <unistd.h>
@@ -59,7 +73,8 @@ APR_DECLARE_OPTIONAL_FN(int, ssl_is_https, (conn_rec *));
 typedef struct {
     int                enable;
     int                timeout;
-    int                maxreqs;
+    int                softReqs;
+    int                hardReqs;
 } fence_server_cfg;
 
 typedef struct {
@@ -90,7 +105,8 @@ static void *fence_create_server_cfg(apr_pool_t *p, server_rec *s)
 
     cfg->enable = 0;
     cfg->timeout = 0;
-    cfg->maxreqs = 0;
+    cfg->softReqs = 0;
+    cfg->hardReqs = 0;
 
     return (void *)cfg;
 }
@@ -108,18 +124,31 @@ static const char *fence_enable(cmd_parms *cmd, void *dummy, int flag)
 }
 /** FENCE_ENABLE **/
 
-/** FENCE_SETMAXREQS **/
-static const char *fence_setmaxreqs(cmd_parms *cmd, void *dummy, const char *arg)
+/** FENCE_SETSOFTREQS **/
+static const char *fence_setSoftReqs(cmd_parms *cmd, void *dummy, const char *arg)
 {
     server_rec *s = cmd->server;
     fence_server_cfg *cfg = (fence_server_cfg *)ap_get_module_config(s->module_config,
                                                                    &fence_module);
 
-    cfg->maxreqs = atoi(arg);
+    cfg->softReqs = atoi(arg);
     return NULL;
 
 }
-/** FENCE_SETMAXREQS **/
+/** FENCE_SETSOFTREQS **/
+
+/** FENCE_SETHARDREQS **/
+static const char *fence_setHardReqs(cmd_parms *cmd, void *dummy, const char *arg)
+{
+    server_rec *s = cmd->server;
+    fence_server_cfg *cfg = (fence_server_cfg *)ap_get_module_config(s->module_config,
+                                                                   &fence_module);
+
+    cfg->hardReqs = atoi(arg);
+    return NULL;
+
+}
+/** FENCE_SETHARDREQS **/
 
 
 /** FENCE_SETTIMTEOUT **/
@@ -133,6 +162,46 @@ static const char *fence_settimeout(cmd_parms *cmd, void *dummy, const char *arg
     return NULL;
 }
 /** FENCE_SETTIMTEOUT **/
+
+/** IS_HOSTNAME_VALID **/
+char is_hostname_valid(const char *hostname)
+{
+  char ret = 1, found = 0;
+  size_t i, i2, len, len2;
+
+  char allowed_chars[] = "QWERTZUIOPASDFGHJKLYXCVBNMqwertzuiopasdfghjklyxcvbnm-_.0123456789:";
+
+  len = strlen(hostname);
+  len2 = strlen(allowed_chars);
+
+  /** AVOID BUFFER OVERFLOW **/
+  if(len > MAX_HOSTNAME_LEN)
+  {
+    return 0;
+  }
+  /** AVOID BUFFER OVERFLOW **/
+
+  for(i = 0; i < len; i++)
+  {
+    found = 0;
+    for(i2 = 0; i2 < len2; i2++)
+    {
+      if(hostname[i] == allowed_chars[i2])
+      {
+        found = 1;
+      }
+    }
+
+    if(!found)
+    {
+      ret = 0;
+      break;      
+    }
+  }
+
+  return ret;
+}
+/** IS_HOSTNAME_VALID **/
 
 /** FENCE_POST_READ_REQUEST **/
 static int fence_post_read_request(request_rec *r) 
@@ -154,10 +223,13 @@ static int fence_post_read_request(request_rec *r)
     static pid_t child_pid;
     worker_score *ws_record = apr_palloc(r->pool, sizeof *ws_record);
     process_score *ps_record;
-    int online_procs = 0;
+    int hard_slot_used = 0;
+    int soft_slot_used = 0;
+    char invalid_hostname = 0;
 
     pid_t *pid_buffer, worker_pid;
     char *stat_buffer;
+    char datebuff[APR_RFC822_DATE_LEN + 1];
 
     int *thread_idle_buffer = NULL;
     int *thread_busy_buffer = NULL;
@@ -166,6 +238,7 @@ static int fence_post_read_request(request_rec *r)
 
     apr_time_t nowtime = apr_time_now();
     tu = ts = tcu = tcs = 0;
+    time_t tst = 0;
 
 
     /** INIT **/
@@ -186,6 +259,126 @@ static int fence_post_read_request(request_rec *r)
         return DECLINED;
     }
 
+    if(!is_hostname_valid(r->hostname))
+    {
+        ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_WARNING, 0, r, "[mod_fence] Invalid Hostname: %s",r->hostname);
+        return 400; //No Fancy response, invalid hostname earns simple 400 ERROR..
+    }
+
+
+    /** ITERATE_CONNECTION_POOL **/
+    for (i = 0; i < server_limit; ++i) 
+    {
+        for (j = 0; j < thread_limit; ++j) 
+        {
+            ap_copy_scoreboard_worker(ws_record, i, j);
+
+            if (ws_record->access_count == 0 && (ws_record->status == SERVER_READY || ws_record->status == SERVER_DEAD)) 
+            {
+                continue;
+            }
+
+            ps_record = ap_get_scoreboard_process(i);
+
+/*              Filter or not to be? - this is the question..
+            if(!strcmp(r->DEF_IP,"127.0.0.1"))
+            {
+                continue; //DONT FILTER LOCALHOST
+            }
+*/
+            if(cfg->softReqs && !strcmp(ws_record->client, r->DEF_IP) && ( ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE || ws_record->status == SERVER_GRACEFUL))
+            {
+                soft_slot_used++;
+            }
+
+            if(cfg->hardReqs && (long)apr_time_sec(nowtime - ws_record->last_used) > 0 && !strcmp(ws_record->client, r->DEF_IP) && ( ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE || ws_record->status == SERVER_GRACEFUL))
+            {
+                hard_slot_used++;
+            }
+
+            /**
+                ap_rprintf(r,
+                           " <i>%s {%s}</i> <i>(%s)</i> <b>[%s]</b><br />\n\n",
+                           ap_escape_html(r->pool,
+                                          ws_record->client),
+                           ap_escape_html(r->pool,
+                                          ap_escape_logitem(r->pool,
+                                                            ws_record->request)),
+                           ap_escape_html(r->pool,
+                                          ws_record->protocol),
+                           ap_escape_html(r->pool,
+                                          ws_record->vhost));
+            **/
+
+
+            /** UNUSED FROM MOD_STATUS **/
+            /*
+            if (ws_record->start_time == 0L)
+                req_time = 0L;
+            else
+                req_time = (long)
+                    ((ws_record->stop_time - ws_record->start_time) / 1000);
+            if (req_time < 0L)
+                req_time = 0L;
+
+            lres = ws_record->access_count;
+            my_lres = ws_record->my_access_count;
+            conn_lres = ws_record->conn_count;
+            bytes = ws_record->bytes_served;
+            my_bytes = ws_record->my_bytes_served;
+            conn_bytes = ws_record->conn_bytes;
+            */
+            /** UNUSED FROM MOD_STATUS **/
+
+            if (ws_record->pid) 
+            { /* MPM sets per-worker pid and generation */
+                worker_pid = ws_record->pid;
+                worker_generation = ws_record->generation;
+            }
+            else 
+            {
+                worker_pid = ps_record->pid;
+                worker_generation = ps_record->generation;
+            }
+
+            if((long)apr_time_sec(nowtime - ws_record->last_used) >= cfg->timeout && ( ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE || ws_record->status == SERVER_GRACEFUL))
+            {
+                if(ps_record->pid > 0)
+                {
+                    kill(ps_record->pid, SIGKILL);
+                    ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, r->server, "[mod_fence] Stucked Child %d, Killed",ps_record->pid);
+                }
+            }
+
+        } 
+    }
+    /** ITERATE_CONNECTION_POOL **/
+
+
+
+    /** MITIGATE **/
+    if(soft_slot_used > cfg->softReqs || hard_slot_used > cfg->hardReqs)
+    {
+        // GIVE some weird summary about mitigation to endusers
+
+        ap_set_content_type(r, "text/html");
+
+        ap_rputs("<html><head>"
+        "<meta name='robots' content='NOINDEX, NOFOLLOW'>"
+        "</meta><title>mod_fence / Request Terminated</title>"
+        "<style>td { border: 1px solid gray; padding: 10px; } </style>"
+        "</head>"
+        "<body style='margin: 20px; padding: 20px; color: #333;'>"
+        "<h1>Your request terminated due to Auto-Mitigation.</h1>"
+        "<br /><br />"
+        "You are reached maximum-allowed request limit at the same time and it's seems like non-usual activity.<br /><br />"
+        "To protect the service you suffered mitigation on your connection rate.<br />"
+        "Please refer the table below of your recent activity that seems currently abnormal<br /><br />"
+        "In any other case please try again later or you can refresh by clicking <a href='' style='color: #333;'><b>here</b></a>.<br /><br />",r);
+
+        /** SOME DETAILS **/
+        ap_rputs("<table style='border: 1px solid gray; width: 80%;'>",r);
+        /** ITERATE_CONNECTION_POOL **/
         for (i = 0; i < server_limit; ++i) 
         {
             for (j = 0; j < thread_limit; ++j) 
@@ -199,96 +392,41 @@ static int fence_post_read_request(request_rec *r)
 
                 ps_record = ap_get_scoreboard_process(i);
 
-
-                if(cfg->maxreqs && !strcmp(ws_record->client, r->DEF_IP) && ( ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE || ws_record->status == SERVER_GRACEFUL))
+                if(!strcmp(ws_record->client, r->DEF_IP) && (ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE))
                 {
-                    online_procs++;
+                    datebuff[0] = 0x00;
+                    apr_rfc822_date(datebuff, ws_record->last_used);
+
+                    ap_rputs("<tr>",r);
+                    ap_rprintf(r, "<td>%s</td> <td>%s</td> <td>%s</td> <td>%s</td>\n",
+                    datebuff,
+                    ap_escape_html(r->pool,ws_record->client),
+                    ap_escape_html(r->pool,r->hostname),
+                    ap_escape_html(r->pool,ap_escape_logitem(r->pool,ws_record->request)));
+
+                    ap_rputs("</tr>",r);
                 }
-
-                /**
-                    ap_rprintf(r,
-                               " <i>%s {%s}</i> <i>(%s)</i> <b>[%s]</b><br />\n\n",
-                               ap_escape_html(r->pool,
-                                              ws_record->client),
-                               ap_escape_html(r->pool,
-                                              ap_escape_logitem(r->pool,
-                                                                ws_record->request)),
-                               ap_escape_html(r->pool,
-                                              ws_record->protocol),
-                               ap_escape_html(r->pool,
-                                              ws_record->vhost));
-                **/
-
-
-                /** UNUSED FROM MOD_STATUS **/
-                /*
-                if (ws_record->start_time == 0L)
-                    req_time = 0L;
-                else
-                    req_time = (long)
-                        ((ws_record->stop_time - ws_record->start_time) / 1000);
-                if (req_time < 0L)
-                    req_time = 0L;
-
-                lres = ws_record->access_count;
-                my_lres = ws_record->my_access_count;
-                conn_lres = ws_record->conn_count;
-                bytes = ws_record->bytes_served;
-                my_bytes = ws_record->my_bytes_served;
-                conn_bytes = ws_record->conn_bytes;
-                */
-                /** UNUSED FROM MOD_STATUS **/
-
-                if (ws_record->pid) 
-                { /* MPM sets per-worker pid and generation */
-                    worker_pid = ws_record->pid;
-                    worker_generation = ws_record->generation;
-                }
-                else 
-                {
-                    worker_pid = ps_record->pid;
-                    worker_generation = ps_record->generation;
-                }
-
-                if((long)apr_time_sec(nowtime - ws_record->last_used) >= cfg->timeout && ( ws_record->status == SERVER_BUSY_READ || ws_record->status == SERVER_BUSY_WRITE || ws_record->status == SERVER_GRACEFUL))
-                {
-                    if(ps_record->pid > 0)
-                    {
-                        ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_NOTICE, 0, r, "Killing Stucked Child: %d", ps_record->pid);
-                        kill(ps_record->pid, SIGKILL);
-                    }
-                }
-
-            } 
+            }
         }
+        ap_rputs("</table>",r);
+        /** SOME DETAILS **/
 
-        if(online_procs > cfg->maxreqs)
-        {
-          /*
-          ap_set_content_type(r, "text/html");
-          char error_page[] = "<html><head>" \
-          "<meta name='robots' content='NOINDEX, NOFOLLOW'>" \
-          "</meta><title>mod_fence / Request Terminated</title>" \
-          "</head>" \
-          "<body style='margin: 20px; padding: 20px; color: #333;'>" \
-          "<h1>Your request terminated due to <b>Denial Of Service</b> Prevention.</h1>" \
-          "<br /><br />" \
-          "You are requested more than %i parallel requests that is seems non-usual activity.<br /><br />" \
-          "In any other case please contact with the server or website owner / administartor.<br /><br />%s" \
-          "</body></html>";
+        ap_rputs(ap_psignature("<br /><br /><hr>\n",r),r);
+        ap_rputs("<i>",r);
+        ap_rputs(VERSION,r);
+        ap_rputs("</i>",r);
 
-          char temp[4096];
-          temp[0] = 0x00;
+        ap_rputs("</body></html>",r);
 
-          sprintf(temp,error_page,cfg->maxreqs,r->server->server_admin);
-          ap_rputs(temp, r);
-          */
+        ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_WARNING, 0, r, "[mod_fence] Mitigation Triggered, dropping request.");
 
-          ap_log_rerror(APLOG_MARK, APLOG_NOERRNO|APLOG_WARNING, 0, r, "Denial Of Service Mitigation Triggered: %d", online_procs);
+        ap_finalize_request_protocol(r);
+        r->output_filters = r->proto_output_filters;
+        apr_hook_deregister_all(); //DONT PASS THIS REQUEST TOWARDS
 
-          return 503;
-
-        }
+        return OK;
+    }
+    /** MITIGATE **/
 
 
     return DECLINED;
@@ -311,8 +449,15 @@ static const command_rec fence_cmds[] = {
                  "Declare the Child Timeout"
                  ),
     AP_INIT_TAKE1(
-                 "Fence_MaxRequests",
-                 fence_setmaxreqs,
+                 "Fence_MitigateSoftRequests",
+                 fence_setSoftReqs,
+                 NULL,
+                 RSRC_CONF,
+                 ""
+                 ),
+    AP_INIT_TAKE1(
+                 "Fence_MitigateHardRequests",
+                 fence_setHardReqs,
                  NULL,
                  RSRC_CONF,
                  ""
